4aea9fe08c14880b58c1fdf102cac84b2ba36e07
JacksonMusee <jacksonmutisya94@gmail.com>
2024-10-10 23:28:11 +0300

    Complete task 3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,6 +1,6 @@
 import { uploadPhoto, createUser } from './utils';
 
-export default async function handleProfileSignup() {
+export function handleProfileSignup() {
   const photoInfo = uploadPhoto();
   const userInfo = createUser();
 
