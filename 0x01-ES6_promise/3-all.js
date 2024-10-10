abe7a494f47a1eff03e0ebbddc51eb9bbde2d085
JacksonMusee <jacksonmutisya94@gmail.com>
2024-10-10 23:31:53 +0300

    Complete task 3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,15 +1,11 @@
 import { uploadPhoto, createUser } from './utils';
 
-export function handleProfileSignup() {
+export default function handleProfileSignup() {
   const photoInfo = uploadPhoto();
   const userInfo = createUser();
 
-  await Promise.all([photoInfo, userInfo]).then((values) => {
-    console.log(values[0].body, values[1].firstName, values[1].lastName);
-    return Promise.resolve({
-      body: values[0].body,
-      firstName: values[1].firstName,
-      lastName: values[1].lastName,
-    });
-  }).catch(() => new Error('Signup system offline'));
+  return Promise
+    .all([photoInfo, userInfo]).then((values) => {
+      console.log(values[0].body, values[1].firstName, values[1].lastName);
+    }).catch(() => new Error('Signup system offline'));
 }
