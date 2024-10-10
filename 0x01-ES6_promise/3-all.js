ff184b3a71fc9ea123919b3ae4fb41a216cd6918
JacksonMusee <jacksonmutisya94@gmail.com>
2024-10-10 23:08:47 +0300

    Complete task 3

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,10 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  const photoInfo = uploadPhoto();
+  const userInfo = createUser();
+
+  Promise.all([photoInfo, userInfo]).then((values) => {
+    console.log(values[0].body, values[1].firstName, values[1].lastName);
+  }).catch(() => new Error('Signup system offline'));
+}
