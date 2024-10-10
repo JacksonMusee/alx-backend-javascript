f994fa66e8e4922342dcd3750e05aa5ff45a81e8
JacksonMusee <jacksonmutisya94@gmail.com>
2024-10-10 22:20:48 +0300

    Complete task 2

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,11 @@
+export default async function handleResponseFromAPI(promise) {
+  await promise
+    .then(() => {
+      console.log('Got a response from the API');
+      return {
+        status: 200,
+        body: 'success',
+      };
+    })
+    .catch(() => new Error());
+}
