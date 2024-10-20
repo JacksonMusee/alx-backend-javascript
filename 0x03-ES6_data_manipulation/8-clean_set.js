a40d8969a64d9057fbd0079b51a861019c1bdfa8
JacksonMusee <jacksonmutisya94@gmail.com>
2024-10-20 17:54:16 +0300

    Complete task 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,7 +1,7 @@
 export default function cleanSet(set, startString) {
   const selectValues = [];
 
-  if (startString.length > 0) {
+  if (startString && startString.length > 0) {
     for (const item of set) {
       if (typeof item === 'string' && item.startsWith(startString)) {
         selectValues.push(item.slice(startString.length));
