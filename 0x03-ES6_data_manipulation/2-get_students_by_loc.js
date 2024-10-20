e171cd85fa25632cb88278fad5aa01f23857ae99
JacksonMusee <jacksonmutisya94@gmail.com>
2024-10-20 14:23:34 +0300

    Complete task 2

---
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -0,0 +1,5 @@
+export default function getStudentsByLocation(studentsArr, city) {
+  if (!Array.isArray(studentsArr)) return [];
+
+  return studentsArr.filter((student) => student.location === city);
+}
