35b6be0509642a65c0e59556e8f5d303177014c9
JacksonMusee <jacksonmutisya94@gmail.com>
2024-11-29 14:51:04 +0300

    Complete task 8

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,41 +1,20 @@
-const chai = require('chai');
-const chaiHttp = require('chai-http');
-const expect = chai.expect;
+const { expect } = require('chai');
+const request = require('request');
 
-chai.use(chaiHttp);  // Using the chai-http plugin to make HTTP requests in tests
+describe('Index page', function () {
+  // Prevent the server from starting multiple times by only starting it once
 
-describe('Index page', () => {
-  let server;
-
-  // Before each test, start the server
-  before((done) => {
-    server = require('./api');  // Import the api.js file which will start the server
-    done();
+  it('should return status code 200', function (done) {
+    request.get('http://localhost:7865', function (err, res, body) {
+      expect(res.statusCode).to.equal(200);
+      done();  // Call done when the test is complete
+    });
   });
 
-  // After all tests, close the server
-  after((done) => {
-    server.close();
-    done();
-  });
-
-  // Test 1: Correct status code
-  it('should return status code 200', (done) => {
-    chai.request(server)
-      .get('/')
-      .end((err, res) => {
-        expect(res).to.have.status(200);
-        done();
-      });
-  });
-
-  // Test 2: Correct result (response body)
-  it('should return the correct result', (done) => {
-    chai.request(server)
-      .get('/')
-      .end((err, res) => {
-        expect(res.text).to.equal('Welcome to the payment system');
-        done();
-      });
+  it('should return the correct result', function (done) {
+    request.get('http://localhost:7865', function (err, res, body) {
+      expect(body).to.equal('Welcome to the payment system');
+      done();  // Call done when the test is complete
+    });
   });
 });
