8350a4f898f697808cfccd6a7c942999909443e1
JacksonMusee <jacksonmutisya94@gmail.com>
2024-12-01 10:41:21 +0300

    Correct task 1 to execute successfully as per test code

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,23 +1,5 @@
 #!/usr/bin/env node
 
-const readline = require('readline');
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
-process.stdout.write('Welcome to Holberton School, what is your name?\n');
-
-rl.question('', (name) => {
-  if (name) {
-  process.stdout.write(`Your name is: ${name}\r`);
-  }
-  rl.close();
-  process.stdout.write('This important software is now closing\n');
-});
-
-/*
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('readable', () => {
@@ -35,4 +17,3 @@ process.stdin.on('end', () => {
 process.stdin.on('error', (err) => {
   process.stderr.write(`An error occurred: ${err.message}\n`);
 });
-*/
